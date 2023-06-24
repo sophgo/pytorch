3816e86e75b989eaa7abf5cb1ee53b9abe7e6b76
Yukio Siraichi <yukio.siraichi@gmail.com>
2023-06-24 09:57:55 -0300

    Translation validator for dynamo guards.
    This PR introduces a translation validator for dynamo guards. In summary, it verifies
    whether the guards issued as Python code are sound, w.r.t the initial guards.
    
    The main changes in this PR are:
    
    - Create an FX graph for dynamic shapes
    - Translate "the original" guards from the FX graph to Z3
    - Check if the guards produced by `produce_guards` are sound w.r.t. the ones from the FX graph
    
    gh-stack version of the PR #101146.
    
    ghstack-source-id: 36b11186271363d1eb9922d62db7dac50fef5409
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/102563

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -989,6 +989,7 @@ class VariableBuilder:
                         constraint_dim=None,
                     ),
                     hint=value,
+                    source=self.source,
                 )
                 self.tx.output.tracked_fakes.append(
                     TrackedFake(wrapped_value, self.source, None)
